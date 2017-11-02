a371bb3c3f04385d00cc01fe11b51c914da20525
shalurai <shaluraics22@gmail.com>
2017-11-02 11:24:41 +0900

    Default Todo list shows

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  state = {
+    tasks:[
+      "finsih react project",
+     "Finish Redux Tutorials",
+     "Learn more about Relay",
+     "Build 5 more React apps",
+     "Review React Component Lifecycle",
+     "Learn Algorithms",
+     "Tweet Progress",
+     "Get a coffee!",
+    "Learn more about React Native"
+]
+  }
   render() {
     return (
       <div className="App">
        <h1 align="center">TODO List </h1>
+       {this.state.tasks.map(task => <p align='center'>{task}</p>)}
       </div>
     );
   }
