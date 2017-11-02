1235e55a2d30164e1aede52b38da30ff56402d47
shalurai <shaluraics22@gmail.com>
2017-11-02 11:58:51 +0900

    Added newItems in Todo List

--- src/App.js
+++ src/App.js
@@ -13,13 +13,32 @@ class App extends Component {
      "Tweet Progress",
      "Get a coffee!",
     "Learn more about React Native"
-]
+],
+newItem:''
   }
+
+handleChange = (newItem) => {
+  this.setState({newItem:newItem.trim()})
+}
+
+updateItem = (newItem) => {
+  console.log(this.state.newItem);
+  this.state.tasks.push(this.state.newItem)
+  this.setState({newItem:''}) //need to update UI
+}
   render() {
     return (
       <div className="App">
        <h1 align="center">TODO List </h1>
-       {this.state.tasks.map(task => <p align='center'>{task}</p>)}
+       <input
+        className ="search"
+        type='text'
+        placeholder='Add New Item'
+        value = {this.state.newItem}
+        onChange = {event => this.handleChange(event.target.value)}
+       />
+       <button onClick = { this.updateItem}>Add New Task</button>
+       {this.state.tasks.map((task, index) => <p align='center' key={index}>{task}</p>)}
       </div>
     );
   }
