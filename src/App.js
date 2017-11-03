04939146f8596c862c8edfcbd0fd76cf79e7ce13
shalurai <shaluraics22@gmail.com>
2017-11-03 22:39:43 +0900

    Added styles

--- src/App.js
+++ src/App.js
@@ -3,91 +3,91 @@ import escapeRegExp from 'escape-string-regexp'
 import TaskDetail from './TaskDetails'
 
 let intialTasks = [
-  "Finish react project",
- "Finish Redux Tutorials",
- "Learn more about Relay",
- "Build 5 more React apps",
- "Review React Component Lifecycle",
- "Learn Algorithms",
- "Tweet Progress",
- "Get a coffee!",
-"Learn more about React Native"
-];
+   "Finish react project",
+   "Finish Redux Tutorials",
+   "Learn more about Relay",
+   "Build 5 more React apps",
+   "Review React Component Lifecycle",
+   "Learn Algorithms",
+   "Tweet Progress",
+   "Get a coffee!",
+   "Learn more about React Native"
+   ];
 
-class App extends Component {
-
+export default class App extends Component {
 
   state = {
-    tasks:intialTasks,
-newItem:'',
-query:''
+        tasks:intialTasks,
+        newItem:'',
+        query:''
+       }
+
+  handleChange = (newItem) => {
+    this.setState({newItem})
   }
 
-handleChange = (newItem) => {
-  this.setState({newItem})
-}
+  updateQuery = (query) => {
+    this.setState({query})
+  }
 
-updateQuery = (query) => {
-  this.setState({query})
-}
-
-updateItem = (newItem) => {
-  console.log(this.state.newItem);
-  this.state.tasks.push(this.state.newItem)
-  this.setState({newItem:''}) //need to update UI
-}
-
-
-handleReset = () => {
-  this.setState({ tasks: intialTasks })
-
-}
-
-handleClear = () => {
-  this.setState({tasks: []})
-}
-
-handleDelete = (tasks) => {this.setState((state) => ({
-   tasks:state.tasks.filter((d) => d !== tasks )
-
-  }))
-}
-  render() {
-    let showingTask
-   let {query, tasks} = this.state;
-
-   if(query){
-     const match = new RegExp(escapeRegExp(query), 'i')
-     showingTask = tasks.filter(task => match.test(task))
+  updateItem = (newItem) => {
+    this.state.tasks.push(this.state.newItem)
+    this.setState({newItem:''})
    }
-  else {
-    showingTask = tasks
+
+  handleReset = () => {
+    this.setState({ tasks: intialTasks })
   }
+
+  handleClear = () => {
+    this.setState({tasks: []})
+  }
+
+  handleDelete = (tasks) => {
+    this.setState((state) => ({
+     tasks:state.tasks.filter((d) => d !== tasks )
+     })
+    )
+  }
+    render() {
+
+      let showingTask
+      let {query, tasks} = this.state;
+
+      if(query){
+        const match = new RegExp(escapeRegExp(query), 'i')
+        showingTask = tasks.filter(task => match.test(task))
+      }
+     else {
+      showingTask = tasks
+     }
 
     return (
-      <div className="App">
-       <h1 align="center">TODO List </h1>
-       <input
-        className ="search"
-        type='text'
-        placeholder='Add New Item'
-        value = {this.state.newItem}
-        onChange = {event => this.handleChange(event.target.value)}
-       />
-       <input
-        type = 'text'
-        placeholder = 'Search the task '
-        value = { this.state.query}
-        onChange = {event => this.updateQuery(event.target.value)}
+       <div className="mainContainer">
+         <h1 align="center"  className = "titles">React TO-DO List App </h1>
+         <h2 className= 'subtitle'> Enhance your productivity </h2>
+         <input
+           className ="newTask"
+           type='text'
+           placeholder='Add New Item'
+           value = {this.state.newItem}
+           onChange = {event => this.handleChange(event.target.value)}
+         />
+        <input
+          className = 'search'
+          type = 'text'
+          placeholder = 'Search the task '
+          value = { this.state.query}
+          onChange = {event => this.updateQuery(event.target.value)}
         />
-       <button onClick = { this.updateItem}>Add New Task</button>
-       <button onClick = { this.handleClear}>Clear the list </button>
-       <button onClick = { this.handleReset}>Reset the list</button>
-       <TaskDetail tasks = {showingTask}
-        handleDelete = { this.handleDelete} />
+        <button  className = 'enter' onClick = { this.updateItem}>Add New Task</button>
+        <TaskDetail
+          tasks = {showingTask}
+          handleDelete = { this.handleDelete}
+        />
+        <button  className = 'clear' onClick = { this.handleClear}>Clear the list </button>
+        <button className = 'reset' onClick = { this.handleReset}>Reset the list</button>
       </div>
     );
   }
 }
-
-export default App;
--- src/TaskDetails.js
+++ src/TaskDetails.js
@@ -2,36 +2,30 @@ import React, { Component } from 'react'
 
 export default class TaskDetail extends Component{
 
-
-
   render(){
     let { tasks, handleDelete } = this.props
     return(
       <div>
-     <h1> Task Detail</h1>
-     <table>
-      <thead>
-      <tr>
-      <td>Number</td>
-      <td>Tasks</td>
-      <td>Actions</td>
-      </tr>
-      </thead>
-      <tbody>
-      { tasks.map((task,index) =>
-        <tr key={index}>
-        <td>Task No {index+1}</td>
-        <td>{task}</td>
-        <td><button onClick ={() => handleDelete(task)}>(X)</button></td>
-        </tr>
-      )}
-
+      <h1 align = 'center'> Task Detail</h1>
+      <table className='taskTable'>
+        <thead>
+          <tr>
+            <td>Number</td>
+            <td>Tasks</td>
+            <td>Actions</td>
+          </tr>
+       </thead>
+       <tbody>
+         { tasks.map((task,index) =>
+           <tr key={index}>
+             <td>Task No {index+1}</td>
+             <td>{task}</td>
+             <td><button  className = 'remove' onClick ={() => handleDelete(task)}>(X)</button></td>
+           </tr>
+          )}
       </tbody>
       </table>
-
-
       </div>
-
     )
   }
 }
